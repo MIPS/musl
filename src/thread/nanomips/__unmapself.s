de931ffac7eb6721004536f08790e814b6b0ba68
Toma Tabacu <toma.tabacu@mips.com>
2018-03-13 13:07:01 +0000

    GT-250: Fix register name typo in __unmapself.s.
    src/thread/nanomips/
      * __unmapself.s: Change $s7 to $a7, as originally intended in aa4cc7f.

--- src/thread/nanomips/__unmapself.s
+++ src/thread/nanomips/__unmapself.s
@@ -2,8 +2,8 @@
 .global __unmapself
 .type   __unmapself,@function
 __unmapself:
-	li   $s7, 215		# munmap
+	li   $a7, 215		# munmap
 	syscall32
 	li   $a0, 0
-	li   $s7, 93		# exit
+	li   $a7, 93		# exit
 	syscall32
