29ad86e5b349006e6189e958cee80f212b814c0b
James Hogan <james.hogan@mips.com>
2018-05-08 16:55:28 +0100

    stat: move statx handling out of __syscall5
    Drop the unpleasant hack in __syscall5 to conditionally convert the
    struct statx produced by the statx system call to a struct stat, and do
    this in the individual wrappers.
    
    This is much more intuitive, less subtle, and may be important in future
    if we ever need a statx wrapper. Most importantly however the wrappers
    themselves are generic (they should really work out of the box on other
    architectures) and __syscall5 is architecture specific.
    
    Signed-off-by: James Hogan <james.hogan@mips.com>

--- src/stat/stat.c
+++ src/stat/stat.c
@@ -12,8 +12,14 @@ int stat(const char *restrict path, struct stat *restrict buf)
 #elif defined(SYS_fstatat)
 	return syscall(SYS_fstatat, AT_FDCWD, path, buf, 0);
 #else
-	return syscall(SYS_statx, AT_FDCWD, path, AT_STATX_SYNC_AS_STAT,
-			STATX_BASIC_STATS, buf);
+	struct statx bufx;
+	int result;
+
+	result = syscall(SYS_statx, AT_FDCWD, path, AT_STATX_SYNC_AS_STAT,
+			 STATX_BASIC_STATS, &bufx);
+	if (!result)
+		__statx_to_stat(&bufx, buf);
+	return result;
 #endif
 }
 
